f70774bba419271c04a2cc213dc1644c0293e800
Santiagood <rainrainsantiago@gmail.com>
2021-11-20 17:32:42 +0800

    database migration

--- firstproject/routes/web.php
+++ firstproject/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PagesController;
+use App\Http\Controllers\PostController;
 use Illuminate\Routing\Route as RoutingRoute;
 
 /*
@@ -18,3 +19,4 @@ use Illuminate\Routing\Route as RoutingRoute;
 //Laravel 8 (new)
 Route::get('/', [PagesController::class, 'index']);
 Route::get('/about', [PagesController::class, 'about']);
+Route::get('/posts', [PostController::class, 'index']);
